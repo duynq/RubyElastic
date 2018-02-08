ed8b725b29d7d4cb1e23d8cfddba41c3d946835e
duynq <quangduyx188@gmail.com>
2018-02-08 11:32:32 +0700

    task-init

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+(1..60000).each do |i|
+  User.create first_name: "Duy_#{i}", last_name: "Nguyen", birth_day: Time.zone.now
+end
